08f3af07445ed25a45954e7e6a9e179dbc0e6951
Yaroslav Erohin <ysoftware@yandex.ru>
2021-01-10 19:32:38 +0300

    Initial Commit

---
+++ ThreadTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadTest
+//
+//  Created by Ерохин Ярослав Игоревич on 10.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
